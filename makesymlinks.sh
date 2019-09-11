4fdc6f4c46e8d3ed52b4c1699f26318573375c7d
Abhay Kumar Singh <abhayawesome@gmail.com>
2019-09-11 10:49:17 +0530

    Change the folder name to tuning

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/tuning                    # dotfiles directory
+olddir=~/dotfiles_tuning_old             # old dotfiles backup directory
 files=".bashrc .vimrc"    # list of files/folders to symlink in homedir
 
 ##########
